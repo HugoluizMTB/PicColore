cf87031e1b28811109ee2369b1404a093d0ce99f
Julio Cesar <julio.souza7@hotmail.com>
2022-10-25 17:56:44 -0300

    remake front script and fix remove button bug

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
-var variavel = 0;
-		function clicou(){
-			var nome = window.document.getElementById('nome');
-			var cpf = window.document.getElementById('cpf');
-			var nomeCrianca = window.document.getElementById('nomeCrianca');
-			var tempo = window.document.getElementById('tempo');
-            var data = new Date;
-            data = data.getHours() + ':' + data.getMinutes()
-			variavel = variavel + 1;
-			if(nome.value == "" || cpf.value == "" || nomeCrianca.value == "" || tempo.value == ""){
-				window.alert("Campos vazios");
-			}
-			else{
-				var tabela = window.document.getElementById('tabela');
-				var linha = tabela.insertRow(variavel);
-				var celula = linha.insertCell(0);
-				var celula1 = linha.insertCell(1);
-				var celula2 = linha.insertCell(2);
-				var celula3 = linha.insertCell(3);
-				var celula4 = linha.insertCell(4);
-				var celula5 = linha.insertCell(5);
-				var celula6 = linha.insertCell(6);
-				celula.innerHTML = variavel;
-				celula1.innerHTML = nome.value;
-				celula2.innerHTML = cpf.value;
-				celula3.innerHTML = nomeCrianca.value;
-				celula4.innerHTML = tempo.value;
-				celula5.innerHTML = data;
-				celula6.innerHTML = "<button onclick='remover()' type='button' class='btn btn-danger'>Remover</button>";
-				nome.value = "";
-				cpf.value = "";
-				nomeCrianca.value = "";
-				tempo.value = "";
-			}
-		}
-		function remover(){
-			var tabela = window.document.getElementById('tabela');
-			var linha = tabela.deleteRow(variavel);
-			variavel = variavel - 1;
-		}
+const formEl = document.querySelector('form')
+const tbodyEl = document.querySelector('tbody')
+const tableEl = document.querySelector('table')
+
+function submitClient(e) {
+
+	e.preventDefault()
+	const nome = document.getElementById('nome').value
+	const cpf = document.getElementById('cpf').value
+	const nomeCrianca = document.getElementById('nomeCrianca').value
+	const tempoDesejado = document.getElementById('tempo').value
+	let horaEntrada = new Date
+	let minuto = ('0' + horaEntrada.getMinutes()).slice(-2)
+	let hora = horaEntrada.getHours()
+	horaEntrada = `${hora}:${minuto}`
+
+	if (nome.value == "" || cpf.value == "" || nomeCrianca.value == "" || tempoDesejado.value == "") {
+		window.alert('Campos Vazios!')
+	} else {
+		tbodyEl.innerHTML += `
+		<tr>
+			<td>${nome}</td>
+			<td>${cpf}</td>
+			<td>${nomeCrianca}</td>
+			<td>${tempoDesejado}</td>
+			<td>${horaEntrada}</td>
+			<td><button class="deleteBtn">Remover</button></td>
+		</tr>
+	`
+	document.querySelector('form').reset()	
+	}
+}
+
+function deleteRow(e) {
+	if (!e.target.classList.contains("deleteBtn")) {
+		return
+	}
+	
+	const btn = e.target
+	btn.closest('tr').remove()
+}
+
+formEl.addEventListener('submit', submitClient)
+tableEl.addEventListener('click', deleteRow)
