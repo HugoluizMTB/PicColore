43e6905df585f7a6a65d920c9f90f89c51e1f806
juliocsx <julio.souza7@hotmail.com>
2023-05-09 15:12:27 -0300

    feat: refactor client controller and services

--- src/service/client.service.ts
+++ src/service/client.service.ts
@@ -1,14 +1,21 @@
-import Active_client from '../schema/active_client.schema'
-import Client from '../schema/client.schema'
+import Client from "../schema/client.schema";
 
-export const checkIfClientIsActive = async (client_id: string) =>  {
-    return await Active_client.findOne({ where: { client_id: client_id } })
-}
+export const createClientService = async (client: any) => {
+  const validateCpf = await Client.findOne({ where: client });
+  if (validateCpf) {
+    return { msg: "Cpf já cadastrado" }
+  };
+  
+  const createClient = await Client.create(client);
+  return createClient;
+};
 
-export const checkIfClientExistsByCPF = async (client_cpf: string) => {
-    return await Client.findOne({ where: { client_cpf: client_cpf }})
-}
+export const getClientByCPFService = async (client: any) => {
+  const getClient = await Client.findOne({ where: client });
+  return getClient;
+};
 
-export const checkIfClientExistsByID = async (client_id: string) => {
-    return await Client.findOne({ where: { client_id: client_id }})
-}
+export const destroyClientByCpfService = async (client: any) => {
+  const destroyClient = await Client.destroy({ where: client });
+  return { msg: "Cliente excluído" };
+};
