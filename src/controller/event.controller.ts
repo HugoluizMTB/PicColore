9a6e7eff25e6b305b29df7ef489b85f0a14c38d3
Julio Cesar <julio.souza7@hotmail.com>
2023-05-12 13:47:14 -0300

    refactor event controller and rename schema columns

--- src/controller/event.controller.ts
+++ src/controller/event.controller.ts
@@ -1,140 +1,32 @@
-import Event from "../schema/event.schema";
 import { Request, Response } from "express";
 import { EventService } from "../service/index.service";
 
 const createEvent = async (req: Request, res: Response) => {
-  try {
-    const body = req.body;
-    const {
-      event_name,
-      event_address,
-      event_status,
-      event_is_freetime_active,
-      event_freetime_price,
-      event_courtesy,
-      event_price_1,
-      event_time_1,
-      event_price_2,
-      event_time_2,
-      event_price_3,
-      event_time_3,
-      event_price_4,
-      event_time_4,
-      event_additional_minute
-    } = body;
-    const checkEventNameAndAddress = EventService.isEventNameAndAddressAvailable(event_name, event_address)
-    if (!checkEventNameAndAddress) {
-      res.status(406).send({ msg: "Nome ou Endereço em uso" });
-    } else {
-      const createEvent = await Event.create({
-        event_name,
-        event_address,
-        event_status,
-        event_is_freetime_active,
-        event_freetime_price,
-        event_courtesy,
-        event_price_1,
-        event_time_1,
-        event_price_2,
-        event_time_2,
-        event_price_3,
-        event_time_3,
-        event_price_4,
-        event_time_4,
-        event_additional_minute
-      });
-      res.status(201).send(createEvent);
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ msg: "Não foi possível completar a requisição" });
-  }
+  const body = req.body;
+  const createdEvent = await EventService.createEventService(body);
+  res.status(201).json(createdEvent);
 };
 
 const getAllEvents = async (req: Request, res: Response) => {
-    try {
-      const AllEvents = await Event.findAll()
-      if (!AllEvents) {
-        res.status(200).send({ msg: "Nenhum evento encontrado" })
-      } else {
-        res.status(200).json(AllEvents)
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(500).send({ msg: "Não foi possível completar a requisição" })
-    }
-  }
+  const AllEvents = await EventService.getAllEventsService();
+  res.json(AllEvents).status(200);
+};
 
 const updateEvent = async (req: Request, res: Response) => {
-  try {
-    const body = req.body;
-    const {
-      event_id,
-      event_name,
-      event_address,
-      event_status,
-      event_is_freetime_active,
-      event_freetime_price,
-      event_courtesy,
-      event_price_1,
-      event_time_1,
-      event_price_2,
-      event_time_2,
-      event_price_3,
-      event_time_3,
-      event_price_4,
-      event_time_4,
-      event_additional_minute
-    } = body;
-    const checkEventNameAndAddress = EventService.isEventNameAndAddressAvailable(event_name, event_address)
-    if (!checkEventNameAndAddress) {
-      res.status(406).send({ msg: "Nome ou Endereço em uso" });
-    } else {
-      const updateEvent = await Event.update({
-        event_name,
-        event_address,
-        event_status,
-        event_is_freetime_active,
-        event_freetime_price,
-        event_courtesy,
-        event_price_1,
-        event_time_1,
-        event_price_2,
-        event_time_2,
-        event_price_3,
-        event_time_3,
-        event_price_4,
-        event_time_4,
-        event_additional_minute
-      }, { where: { event_id } });
-      const UpdatedEvent = await Event.findOne({ where: { event_id } });
-      res.status(200).send(UpdatedEvent);
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ msg: "Não foi possível completar a requisição" });
-  }
+  const body = req.body;
+  const updatedEvent = await EventService.updateEventService(body);
+  res.json(updatedEvent).status(200);
 };
 
 const destroyEvent = async (req: Request, res: Response) => {
-  try {
-    const body = req.body
-    const { event_id } = body
-    const destroyedEvent = await Event.destroy({ where: { event_id } })
-    if (!destroyedEvent) {
-      res.status(204).send({ msg: "Nenhum Evento encontrado!" })
-    } else {
-      res.status(204).json(destroyedEvent)
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ msg: "Não foi possível completar a requisição" })
-  }
-}
+  const body = req.body;
+  const destroyedEvent = await EventService.destroyEventService(body);
+  res.json(destroyedEvent).status(204);
+};
 
 export { 
-    createEvent,
-    getAllEvents,
-    updateEvent,
-    destroyEvent
+  createEvent, 
+  getAllEvents, 
+  updateEvent, 
+  destroyEvent 
 };
--- src/service/event.service.ts
+++ src/service/event.service.ts
@@ -1,11 +1,80 @@
-import Event from '../schema/event.schema'
+import { Op } from "sequelize";
+import Event from "../schema/event.schema";
 
-export const isEventNameAndAddressAvailable = async (event_name: string, event_address: string) =>  {
-    const checkName = await Event.findOne({ where: { event_name: event_name } })
-    const checkAddress = await Event.findOne({ where: { event_address: event_address } })
-    if (checkName || checkAddress) {
-        return false
-    } else {
-        return true
+const isEventNameAndAddressAvailable = async (event_name: string, event_address: string) => {
+  const checkNameAndAddress = await Event.findOne({ where: {[Op.or]: [{ name: event_name }, { address: event_address }]}});
+  return checkNameAndAddress;
+};
+
+export const createEventService = async (event: any) => {
+  try {
+    const { name, address } = event;
+    const checkEventNameAndAddress = await isEventNameAndAddressAvailable(name, address);
+    if (checkEventNameAndAddress) {
+      return { msg: "Nome ou Endereço em uso" };
     }
-}
+    const createEvent = await Event.create(event);
+    return createEvent;
+  } catch (error) {
+    console.log(error);
+    return { msg: "Não foi possível completar a requisição" };
+  }
+};
+
+export const getAllEventsService = async () => {
+  try {
+    const allEvents: any = await Event.findAll();
+    if (allEvents.length === 0) {
+      return { msg: "Nenhum evento encontrado" }
+    }
+    
+    return allEvents;
+
+  } catch (error) {
+    console.log(error);
+    return { msg: "Não foi possível completar a requisição" };
+  }
+};
+
+export const updateEventService = async (body: any) => {
+  try {
+    const { name, address, id } = body;
+
+    const checkEventNameAndAddress = await isEventNameAndAddressAvailable(name, address);
+    if (checkEventNameAndAddress) {
+      return { msg: "Nome ou Endereço em uso" };
+    }
+
+    const checkEventId = await Event.findOne({ where: { id } });
+    if (!checkEventId) {
+      return { msg: "Evento não existe" }
+    }
+
+    const updatedEvent = await Event.update( body , { where: { id } });
+    return updatedEvent;
+
+  } catch (error) {
+    console.log(error);
+    return { msg: "Não foi possível completar a requisição" };
+  }
+};
+
+export const destroyEventService = async (body: any) => {
+  try {
+    const { id } = body;
+
+    const checkEventId = await Event.findOne({ where: { id } });
+    if (!checkEventId) {
+      return { msg: "Evento não existe" }
+    }
+
+    const destroyedEvent = await Event.destroy({ where: { id } });
+    if (destroyedEvent) {
+      return { msg: "Evento apagado" };
+    }
+
+  } catch (error) {
+    console.log(error);
+    return { msg: "Não foi possível completar a requisição" };
+  }
+};
